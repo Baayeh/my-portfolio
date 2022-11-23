dc8549bb30a89fd4e5385a2ad0832bb08d072467
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-23 11:00:49 +0000

    Add new project

--- src/components/ProjectArray.js
+++ src/components/ProjectArray.js
@@ -1,6 +1,15 @@
 const projectsArr = [
   {
     id: 1,
+    title: 'COVID-19 Tracker',
+    image: '/assets/covid-tracker.png',
+    description: 'COVID-19 Tracker is a web app that tracks COVID-19 cases worldwide. It uses the VACCOVID API from RapidAPI',
+    languages: ['react', 'redux', 'rest-api', 'tailwindcss'],
+    demo: 'https://covid-metrics.vercel.app/',
+    source: 'https://github.com/Baayeh/covid-metrics'
+  },
+  {
+    id: 2,
     title: 'Rest-Countries',
     image: '/assets/rest-img.jpg',
     description: 'The Rest-Countries App is a website that displays a list of countries from an API. On the site, you are able to see more details about the country, search for a country and also filter by region. Built with ❤',
@@ -9,16 +18,16 @@ const projectsArr = [
     source: 'https://github.com/Baayeh/rest-countries'
   },
   {
-    id: 2,
+    id: 3,
     title: 'Math Magician',
     image: '/assets/math-magician.png',
     description: '"Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to make simple calculations and read a random math-related quote.',
-    languages: ['React', 'Tailwindcss'],
+    languages: ['react', 'tailwindcss'],
     demo: 'https://maths-magicians.vercel.app/',
     source: 'https://github.com/Baayeh/maths-magicians'
   },
   {
-    id: 3,
+    id: 4,
     title: 'Leaderboard',
     image: '/assets/leaderboard.PNG',
     description: 'The leaderboard website displays scores submitted by different players. It also allows you to submit your score. I created this project using ES6 concepts. I connected to the Leadboard API service',
@@ -27,7 +36,7 @@ const projectsArr = [
     source: 'https://github.com/Baayeh/leaderboard'
   },
   {
-    id: 4,
+    id: 5,
     title: 'Todo List',
     image: '/assets/todos.PNG',
     description: 'A Todo App with basic CRUD functionalities using ES6 concepts and webpack. It uses the localStorage to store the data.',
